eaa3cab77ce37ca80a796d32f4fb60679253f1a1
Jesse-Tingle <jtingle0@gmail.com>
2020-03-12 01:07:21 -0400

    add findById and findSteps in scheme-model file

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,6 +4,22 @@ function find() {
 	return db("schemes");
 }
 
+function findById(id) {
+	return db("schemes")
+		.where({ id })
+		.first();
+}
+
+function findSteps(id) {
+	return db("schemes")
+		.join("steps", "steps.scheme_id", "schemes.id")
+		.where("schemes.id", id)
+		.select("schemes.scheme_name", "steps.step_number", "steps.instructions")
+		.orderBy("steps.step_number");
+}
+
 module.exports = {
-	find
+	find,
+	findById,
+	findSteps
 };
